1f2188826b17034cadd5f3fb1adbecdf818c606e
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-01-07 19:31:32 +0600

    vitepress added

---
+++ .vitepress/config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Md Atiquzzaman Soikat",
+  description: "Personal Website of Md Atiquzzaman Soikat, a software developer",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: 'About', link: '/about' },
+      { text: 'Notes', link: '/notes' }
+    ],
+
+    sidebar: {
+        '/notes':       {
+            text: 'Notes',
+            items: [
+              { text: 'Markdown Examples', link: '/markdown-examples' },
+              { text: 'Runtime API Examples', link: '/api-examples' }
+            ]
+          }
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
---
+++ about.md
@@ -0,0 +1,3 @@
+# About Page
+
+This is a about page created using Vitepress.
