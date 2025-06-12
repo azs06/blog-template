644d4d7522caa3fdac14badf5499b41acdc78830
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-06-12 12:26:10 +0600

    fixe accessibility issue

--- .vitepress/theme/Footer.vue
+++ .vitepress/theme/Footer.vue
@@ -1,41 +1,51 @@
 <template>
   <footer>
     <div class="footer-container">
-      <p class="copyright">Copyright &copy; 2021</p>
-      <nav class="footer-links">
+      <p class="copyright">Copyright &copy; {{ currentYear }}</p>
+      <nav class="footer-links" aria-label="Social media links">
         <ul>
           <li>
-            <a class="icon-container" href=""
-              ><img class="icon" src="./assets/icons/github.svg" alt=""
-            /></a>
+            <a class="icon-container" href="https://github.com/username" aria-label="GitHub">
+              <img class="icon" src="./assets/icons/github.svg" alt="GitHub logo">
+            </a>
           </li>
           <li>
-            <a class="icon-container" href=""
-              ><img class="icon" src="./assets/icons/stackoverflow.svg" alt=""
-            /></a>
+            <a class="icon-container" href="https://stackoverflow.com/users/username" aria-label="Stack Overflow">
+              <img class="icon" src="./assets/icons/stackoverflow.svg" alt="Stack Overflow logo">
+            </a>
           </li>
           <li>
-            <a class="icon-container" href=""
-              ><img class="icon" src="./assets/icons/twitter.svg" alt=""
-            /></a>
+            <a class="icon-container" href="https://twitter.com/username" aria-label="Twitter">
+              <img class="icon" src="./assets/icons/twitter.svg" alt="Twitter logo">
+            </a>
           </li>
           <li>
-            <a class="icon-container" href=""
-              ><img class="icon" src="./assets/icons/freecodecamp.svg" alt=""
-            /></a>
+            <a class="icon-container" href="https://freecodecamp.org/username" aria-label="freeCodeCamp">
+              <img class="icon" src="./assets/icons/freecodecamp.svg" alt="freeCodeCamp logo">
+            </a>
           </li>
           <li>
-            <a class="icon-container" href=""
-              ><img class="icon" src="./assets/icons/codepen.svg" alt=""
-            /></a>
+            <a class="icon-container" href="https://codepen.io/username" aria-label="CodePen">
+              <img class="icon" src="./assets/icons/codepen.svg" alt="CodePen logo">
+            </a>
           </li>
           <li>
-            <a class="icon-container" href=""
-              ><img class="icon" src="./assets/icons/gmail.svg" alt=""
-            /></a>
+            <a class="icon-container" href="mailto:email@example.com" aria-label="Email">
+              <img class="icon" src="./assets/icons/gmail.svg" alt="Email icon">
+            </a>
           </li>
         </ul>
       </nav>
     </div>
   </footer>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentYear: new Date().getFullYear()
+    }
+  }
+}
+</script>
--- index.md
+++ index.md
@@ -2,35 +2,35 @@
 layout: home
 ---
 <div class="container">
-        <section class="hero-section">
+        <section class="hero-section" aria-label="About section">
           <figure class="image-container">
             <img
               src="https://avatars.githubusercontent.com/u/1593663?v=4"
-              alt=""
+              alt="Author's portrait"
             />
           </figure>
           <div class="hero-section__intro">
-            <h2 class="hero-section__intro-hello">
+            <h1 class="hero-section__intro-hello">
               About Me
-            </h2>
+            </h1>
             <div class="hero-section__intro-text">
               <p>
                 I’m a Software Developer from Dhaka, Bangladesh. Currently
-                working as a Lead Developer at <a>Skipper Hospitality</a>, New
-                York. I co - founded ReformedTech, a startup in Dhaka,
+                working as a Lead Developer at <a href="https://skipperhospitality.com">Skipper Hospitality</a>, New
+                York. I co-founded ReformedTech, a startup in Dhaka,
                 Bangladesh.
               </p>
               <p>
                 My core skills are in JavaScript, Node.js, HTML, CSS, Vue.js and
                 React. I have a strong background in PHP, MySQL, and WordPress.
                 I have strong knowledge of the
-                <b>Frontend Application Architecture.</b> Most recently, I have
-                been dabbling on the world of <b>Jamstack</b>, Developing high
+                <strong>Frontend Application Architecture.</strong> Most recently, I have
+                been dabbling in the world of <strong>Jamstack</strong>, developing high
                 performance, scalable, and maintainable static websites for
                 clients.
               </p>
               <em
-                >Currently, It's my goal to write a blog about the things I
+                >Currently, my goal is to write a blog about the things I
                 learn and do regularly.</em
               >
             </div>
